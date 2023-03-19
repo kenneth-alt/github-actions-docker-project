b8697b413c37d51dbcdd0e83870fe7e875bad39a
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-19 14:17:26 -0600

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello, this is the root directory" }');
+});
+
+app.get('/path1', function (req, res) {
+    res.send('{ "response": "Hello World, this is a new path" }');
+});
+app.get('/path2', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+
+app.listen(process.env.PORT || 3000);
+
+exports = { app }
